67c5c635153bb8e04abe585b08b2b50c957ec540
Hadrien <smethadrien.96@gmail.com>
2023-04-19 14:41:13 +0200

    Final commit before deploying on netlify. Fingers crossed as always. I add a Readme and I changed the URL for the API

--- src/utils/variables.js
+++ src/utils/variables.js
@@ -1,6 +1,6 @@
 export const process = {
     env: {
-        REACT_APP_API_URL: "http://localhost:3000/",
+        REACT_APP_API_URL: "https://travel-app-server.herokuapp.com/",
         REACT_APP_ADMIN_ID: "643fc6a1ad463aa9ca6c742b",
     },
 };
